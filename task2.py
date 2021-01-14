1e3f80b07fab7a48e29116c1d6a57b2fb4e87991
Sebkd <sebkd@mail.ru>
2021-01-14 20:45:14 +0700

    final coding task2

--- task2.py
+++ task2.py
@@ -9,9 +9,8 @@ if os.path.isfile(r'text2.txt'):
     os.remove('text2.txt')
 
 with open(r'text2.txt', 'a+', encoding = 'utf-8') as f_obj:
-    f_obj.write('12334223\n')
-    f_obj.write('vdbvabdvkb\n')
-
+    f_obj.write('1234525252556789\n')
+    f_obj.write('q355125545werty\n')
     f_obj.seek (0)
-    for line in f_obj:
-        print (line.strip())
+    for index, line in enumerate(f_obj, start = 1):
+        print(f'Строка = {index}, Количество букв строке = {len(line)-1}')
