18103e5683e9e267a4919639bc29faafb5b6bdf7
Sebkd <sebkd@mail.ru>
2021-01-15 19:28:49 +0700

    final task3

--- task3.py
+++ task3.py
@@ -4,3 +4,15 @@
 Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 Выполнить подсчет средней величины дохода сотрудников.
 '''
+
+import os.path
+print(f'Фамилии сотрудников, зарплата которых больше 20 000 руб:')
+with open(r'salary.txt', 'a+', encoding = 'utf-8') as f_obj:
+    f_obj.seek(0)
+    average_salary, count = 0, 0
+    for line in f_obj:
+        if float(line.split ()[1]) > 20000:
+            count +=1
+            average_salary +=float(line.split()[1])
+            print(line.split()[0])
+print(f'Средняя зарплата этих сотрудников: {round((average_salary / count), 2)}')
