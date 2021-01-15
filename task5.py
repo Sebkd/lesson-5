d2a05d588f563ceabbef6de2c4086b73230b3249
Sebkd <sebkd@mail.ru>
2021-01-15 23:10:03 +0700

    final task5

--- task5.py
+++ task5.py
@@ -4,7 +4,17 @@
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 '''
 import os.path
+import random
 
 if os.path.isfile(r'text5.txt'):
-    os.remove('text5txt')
+    os.remove('text5.txt')
 
+original_list = [element for element in range(random.randint(1, 10))]
+random.shuffle(original_list)
+print(f'Случайный набор чисел списка: {original_list}')
+
+with open(r'text5.txt', 'a+', encoding = 'utf-8') as f_obj:
+    for line in original_list:
+        f_obj.write(''.join(str(line) + ' '))
+    f_obj.seek(0)
+    print(f'Сумма чисел в файле = {sum(map(int, f_obj.readline().split()))}')
